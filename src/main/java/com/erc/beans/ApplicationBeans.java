399de0b33b2c8e8c884aa5b0b5d3df5e19d2a510
AleksandrShatov <shatov.alexman@gmail.com>
2021-07-25 17:14:45 +0300

    Security init

--- src/main/java/com/erc/beans/ApplicationBeans.java
+++ src/main/java/com/erc/beans/ApplicationBeans.java
@@ -1,8 +1,14 @@
 package com.erc.beans;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ApplicationBeans {
 
+    @Bean
+    public NoOpPasswordEncoder noOpPasswordEncoder() { // Исключительно в образовательных целоях
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
 }
